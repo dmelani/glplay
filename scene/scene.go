1488a70c7c1e6ef1f4c0c4615d77859fa17065e4
Daniel Melani <daniel.melani@gmail.com>
2014-06-08 15:49:14 +0200

    Add some default shaders.
    Not loading them yet though

--- scene/scene.go
+++ scene/scene.go
@@ -9,6 +9,8 @@ type Scene struct {
 	width int
 	height int
 	rectangle *models.Rectangle
+	vertex_shader string
+	fragment_shader string
 }
 
 func Create(width int, height int) (scene *Scene) {
@@ -16,6 +18,27 @@ func Create(width int, height int) (scene *Scene) {
 		width: width,
 		height: height,
 		rectangle: models.NewRectangle(-0.5, -0.5, 1.0, 1.0),
+		vertex_shader: `
+			#version 150
+
+			in vec3 vert;
+
+			void main() {
+				// does not alter the vertices at all
+				gl_Position = vec4(vert, 1);
+			}
+		`,
+		fragment_shader: `
+			#version 150
+
+			out vec4 finalColor;
+
+			void main() {
+				//set every drawn pixel to white
+				finalColor = vec4(1.0, 1.0, 1.0, 1.0);
+			}
+		`,
+
 	}
 
 	return scene
